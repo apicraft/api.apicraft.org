8903424475043b0e015a9fd912b7072d6a1b17f0
Kevin Swiber <kswiber@gmail.com>
2013-03-01 12:18:39 -0500

    Changing default local port to 3001.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 function Config() {
-  this.port = process.env.PORT || 3000;
+  this.port = process.env.PORT || 3001;
   this.baseHrefUri = process.env.BASE_HREF_URI || 'http://localhost:' + this.port;
   this.baseRelUri = process.env.BASE_REL_URI || 'http://rels.api.apicraft.org';
 }
