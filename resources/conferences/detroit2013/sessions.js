d78c5e76d71ac984f066965f2bd192358aa57b17
Kevin Swiber <kswiber@gmail.com>
2013-04-11 13:33:44 -0400

    Adding a missing period.

--- resources/conferences/detroit2013/sessions.js
+++ resources/conferences/detroit2013/sessions.js
@@ -1,7 +1,7 @@
 module.exports = function(addHandler) {
   addHandler('request', function(env, next) {
   	var sessions = {};
-    sessions.sessions = [{"description":"Sessions will be set by the attendees on Day 1.  Proceedings will be posted after the conference"}];
+    sessions.sessions = [{"description":"Sessions will be set by the attendees on Day 1.  Proceedings will be posted after the conference."}];
     sessions.links = [
       { rel: 'self', href: env.helpers.uri('/conferences/' + env.config.location + env.request.url) },
       { rel: 'index', href: env.helpers.uri('/conferences/') + env.config.location }
