1896d91d9c8e3036f4dbb3241e719115b262d865
landlessness <brian@landlessness.net>
2014-07-21 15:33:59 -0400

    adding second venue to venues part 2

--- resources/conferences/detroit2014/home.js
+++ resources/conferences/detroit2014/home.js
@@ -1,43 +1,68 @@
 module.exports = function(addHandler) {
-  addHandler('request', function(env, next) {
-    var body = [{
-      name: 'API Craft Conference',
-      version: '2.0',
-      start: 'Tue Jul 29 2014 20:00:00 GMT-0400 (EDT)',
-      end: 'Wed Jul 30 2014 19:00:00 GMT-0400 (EDT)',
-      theme: 'Where do we lead the APIs of tomorrow?',
-      image: 'port-detroit.jpg',
-      location: {
-        display_name:'Port Detroit',
-        city: 'Detroit',
-        display_address: [ '130 E Atwater St', 'Downtown Detroit', 'Detroit, MI 48226' ],
-        geo_accuracy: 8,
-        neighborhoods: [ 'Downtown Detroit' ],
-        postal_code: '48226',
-        country_code: 'US',
-        address: [ '130 E Atwater St' ],
-        coordinate: { latitude:  42.327278, longitude: -83.041668 },
-        state_code: 'MI'
-      },
-      format: 'Open Space <http://en.wikipedia.org/wiki/Open-space_technology>',
-      links: [
-        { rel: 'self', href: env.helpers.uri('/conferences/') + env.config.location }
-      ]
-    }];
+	addHandler('request', function(env, next) {
+		var body = {
+			venues: [
+			{ 
+				name: 'API Craft Conference',
+				version: '2.0',
+				start: 'Tue Jul 29 2014 20:00:00 GMT-0400 (EDT)',
+				end: 'Wed Jul 30 2014 19:00:00 GMT-0400 (EDT)',
+				theme: 'Where do we lead the APIs of tomorrow?',
+				image: 'port-detroit.jpg',
+				location: {
+					display_name:'Port Detroit',
+					city: 'Detroit',
+					display_address: [ '130 E Atwater St', 'Downtown Detroit', 'Detroit, MI 48226' ],
+					geo_accuracy: 8,
+					neighborhoods: [ 'Downtown Detroit' ],
+					postal_code: '48226',
+					country_code: 'US',
+					address: [ '130 E Atwater St' ],
+					coordinate: { latitude:  42.327278, longitude: -83.041668 },
+					state_code: 'MI'
+				},
+				format: 'Open Space <http://en.wikipedia.org/wiki/Open-space_technology>',
+			},
+			{ 
+				name: 'Hypermedia Panel & Hackathon',
+				version: '1.0',
+				start: 'Mon Jul 28 2014 13:00:00 GMT-0400 (EDT)',
+				end: 'Mon Jul 28 2014 20:00:00 GMT-0400 (EDT)',
+				theme: 'Where do we lead hypermedia APIs of tomorrow?',
+				image: 'grand-circus.jpg',
+				location: {
+					display_name:'Grand Circus',
+					city: 'Detroit',
+					display_address: [ '1570 Woodward Ave', 'Downtown Detroit', 'Detroit, MI 48226' ],
+					geo_accuracy: 8,
+					neighborhoods: [ 'Downtown Detroit' ],
+					postal_code: '48226',
+					country_code: 'US',
+					address: [ '1570 Woodward Ave' ],
+					coordinate: { latitude:  42.335866, longitude: -83.049689 },
+					state_code: 'MI'
+				},
+				format: 'Open Space <http://en.wikipedia.org/wiki/Open-space_technology>',
+			}
+			]
+			links: [
+			{ rel: 'self', href: env.helpers.uri('/conferences/') + env.config.location }
+			]
+		};
 
-    var rels = ['goals', 'guidelines', 'agenda', 'attendees',
-                'hotels', 'sessions', 'parties', 'questions', 'transit'];
+		var rels = ['goals', 'guidelines', 'agenda', 'attendees',
+		'hotels', 'sessions', 'parties', 'questions', 'transit'];
 
-    rels.forEach(function(rel) {
-      body.links.push({
-        rel: env.helpers.rel(rel),
-        href: env.helpers.uri('/conferences/' + env.config.location + '/' + rel)
-      });
-    });
+		rels.forEach(function(rel) {
+			body.links.push({
+				rel: env.helpers.rel(rel),
+				href: env.helpers.uri('/conferences/' + env.config.location + '/' + rel)
+			});
+		});
 
-    body.links.push({ rel: 'index', href: env.helpers.uri('home') });
+		body.links.push({ rel: 'index', href: env.helpers.uri('home') });
 
-    env.response.body = body;
-    next(env);
-  });
+		env.response.body = body;
+		next(env);
+	});
 };
